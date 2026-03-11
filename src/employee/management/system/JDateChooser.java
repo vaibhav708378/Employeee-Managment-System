6194df614c2abb75bc23710047fa20e26591a27e
Vaibhav <vaibhav708378@gmail.com>
2026-03-11 17:59:46 +0530

    Employeee Managment System

---
+++ src/employee/management/system/JDateChooser.java
@@ -0,0 +1,26 @@
+package employee.management.system;
+
+import java.awt.Color;
+
+public class JDateChooser {
+
+    public void setBounds(int i, int j, int k, int l) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBounds'");
+    }
+
+    public void setBackground(Color color) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBackground'");
+    }
+
+    public Object getDateEditor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDateEditor'");
+    }
+
+    public Object getUiComponent() {
+        throw new UnsupportedOperationException("Unimplemented method 'getUiComponent'");
+    }
+
+}
---
+++ src/employee/management/system/conn.java
@@ -0,0 +1,28 @@
+package employee.management.system;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conn {
+    
+    Connection connection;
+    Statement statement;
+
+    public conn() {
+        try{
+            Class.forName( "com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagement","root", "123456789");
+            Statement statement = (Statement) connection.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public Object executeQuery() {
+        throw new UnsupportedOperationException("Unimplemented method 'executeQuery'");
+    }
+
+
+}
