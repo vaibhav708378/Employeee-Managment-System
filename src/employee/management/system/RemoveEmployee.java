11e46bdf467c2242a5cf86eb4a69ed56dd84cc67
Vaibhav <vaibhav708378@gmail.com>
2026-03-19 20:46:59 +0530

    Remove Employee Files Changed

--- src/employee/management/system/RemoveEmployee.java
+++ src/employee/management/system/RemoveEmployee.java
@@ -4,197 +4,145 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.sql.ResultSet;
 
 public class RemoveEmployee extends JFrame implements ActionListener {
-    Random ran = new Random();
-    int number = ran.nextInt(999999);
-    JTextField tname, tfname, taddress,tphone, taadhar, temail, tsalary,tdesignation;
-    JLabel tempid;
-    Component tdob;
-
-    JButton add,back;
-
-    JComboBox Boxeducation;
+    Choice choiceEMPID;
+    JButton delete, back;
     RemoveEmployee(){
 
-        getContentPane().setBackground(new Color(163,255,188));
+        JLabel label = new JLabel("Employee ID");
+        label.setBounds(50,50,100,30);
+        label.setFont(new Font("Tahoma", Font.BOLD,15));
+        add(label);
 
-        JLabel heading = new JLabel("Add Employee Detail");
-        heading.setBounds(320,30,500,50);
-        heading.setFont(new Font("serif", Font.BOLD,25));
-        add(heading);
+        choiceEMPID = new Choice();
+        choiceEMPID.setBounds(200,50,150,30);
+        add(choiceEMPID);
 
-        JLabel name = new JLabel("Name");
-        name.setBounds(50,150,150,30);
-        name.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(name);
+        try{
+            conn c = new conn();
+            ResultSet resultSet = c.statement.executeQuery("select * from employee");
+            while (resultSet.next()){
+                choiceEMPID.add(resultSet.getString("empId"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        tname = new JTextField();
-        tname.setBounds(200,150,150,30);
-        tname.setBackground(new Color(177,252,197));
-        add(tname);
+        JLabel labelName = new JLabel("Name");
+        labelName.setBounds(50,100,100,30);
+        labelName.setFont(new Font("Tahoma", Font.BOLD,15));
+        add(labelName);
 
-        JLabel fname = new JLabel("Father's Name");
-        fname.setBounds(400,150,150,30);
-        fname.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(fname);
+        JLabel textName = new JLabel();
+        textName.setBounds(200,100,100,30);
+        add(textName);
 
-        tfname = new JTextField();
-        tfname.setBounds(600,150,150,30);
-        tfname.setBackground(new Color(177,252,197));
-        add(tfname);
+        JLabel labelPhone = new JLabel("Phone");
+        labelPhone.setBounds(50,150,100,30);
+        labelPhone.setFont(new Font("Tahoma", Font.BOLD,15));
+        add(labelPhone);
 
-        JLabel dob = new JLabel("Date Of Birth");
-        dob.setBounds(50,200,150,30);
-        dob.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(dob);
+        JLabel textPhone = new JLabel();
+        textPhone.setBounds(200,150,100,30);
+        add(textPhone);
 
-        
-        tdob.setBounds(200,200,150,30);
-        tdob.setBackground(new Color(177,252,197));
-        add(tdob);
+        JLabel labelemail = new JLabel("Email");
+        labelemail.setBounds(50,200,100,30);
+        labelemail.setFont(new Font("Tahoma", Font.BOLD,15));
+        add(labelemail);
 
-        JLabel salary = new JLabel("Salary");
-        salary.setBounds(400,200,150,30);
-        salary.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(salary);
+        JLabel textEmail = new JLabel();
+        textEmail.setBounds(200,200,100,30);
+        add(textEmail);
 
-        tsalary = new JTextField();
-        tsalary.setBounds(600,200,150,30);
-        tsalary.setBackground(new Color(177,252,197));
-        add(tsalary);
+        try {
+            conn c = new conn();
+            ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '"+choiceEMPID.getSelectedItem()+"'");
+            while (resultSet.next()){
+                textName.setText(resultSet.getString("name"));
+                textPhone.setText(resultSet.getString("phone"));
+                textEmail.setText(resultSet.getString("email"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        JLabel address = new JLabel("Address");
-        address.setBounds(50,250,150,30);
-        address.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(address);
+        choiceEMPID.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                try{
+                    conn c = new conn();
+                    ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '"+choiceEMPID.getSelectedItem()+"'");
+                    while (resultSet.next()) {
+                        textName.setText(resultSet.getString("name"));
+                        textPhone.setText(resultSet.getString("phone"));
+                        textEmail.setText(resultSet.getString("email"));
+                    }
+                }catch (Exception E){
+                    E.printStackTrace();
+                }
+            }
+        });
 
-        taddress= new JTextField();
-        taddress.setBounds(200,250,150,30);
-        taddress.setBackground(new Color(177,252,197));
-        add(taddress);
+        delete = new JButton("Delete");
+        delete.setBounds(80,300,100,30);
+        delete.setBackground(Color.black);
+        delete.setForeground(Color.WHITE);
+        delete.addActionListener(this);
+        add(delete);
 
-
-        JLabel phone = new JLabel("Phone");
-        phone.setBounds(400,250,150,30);
-        phone.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(phone);
-
-        tphone= new JTextField();
-        tphone.setBounds(600,250,150,30);
-        tphone.setBackground(new Color(177,252,197));
-        add(tphone);
-
-        JLabel email = new JLabel("Email");
-        email.setBounds(50,300,150,30);
-        email.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(email);
-
-        temail= new JTextField();
-        temail.setBounds(200,300,150,30);
-        temail.setBackground(new Color(177,252,197));
-        add(temail);
-
-        JLabel education = new JLabel("Higest Education");
-        education.setBounds(400,300,150,30);
-        education.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(education);
-
-        String items[] = {"BBA","B.Tech","BCA", "BA", "BSC", "B.COM", "MBA", "MCA", "MA", "MTech", "MSC", "PHD"};
-        Boxeducation = new JComboBox(items);
-        Boxeducation.setBackground(new Color(177,252,197));
-        Boxeducation.setBounds(600,300,150,30);
-        add(Boxeducation);
-
-        JLabel aadhar = new JLabel("Aadhar Number");
-        aadhar.setBounds(400,350,150,30);
-        aadhar.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(aadhar);
-
-        taadhar= new JTextField();
-        taadhar.setBounds(600,350,150,30);
-        taadhar.setBackground(new Color(177,252,197));
-        add(taadhar);
-
-
-        JLabel empid = new JLabel("Employee ID");
-        empid.setBounds(50,400,150,30);
-        empid.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(empid);
-
-        tempid= new JLabel(""+number);
-        tempid.setBounds(200,400,150,30);
-        tempid.setFont(new Font("SAN_SARIF", Font.BOLD,20));
-        tempid.setForeground(Color.RED);
-        add(tempid);
-
-        JLabel designation = new JLabel("Designation");
-        designation.setBounds(50,350,150,30);
-        designation.setFont(new Font("SAN_SERIF", Font.BOLD,20));
-        add(designation);
-
-        tdesignation= new JTextField();
-        tdesignation.setBounds(200,350,150,30);
-        tdesignation.setBackground(new Color(177,252,197));
-        add(tdesignation);
-
-        add = new JButton("ADD");
-        add.setBounds(450,550,150,40);
-        add.setBackground(Color.black);
-        add.setForeground(Color.WHITE);
-        add.addActionListener(this);
-        add(add);
-
-        back = new JButton("BACK");
-        back.setBounds(250,550,150,40);
+        back = new JButton("Back");
+        back.setBounds(220,300,100,30);
         back.setBackground(Color.black);
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/delete.png"));
+        Image i2 = i1.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel img = new JLabel(i3);
+        img.setBounds(700,80,200,200);
+        add(img);
 
-        setSize(900,700);
+        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons/rback.png"));
+        Image i22 = i11.getImage().getScaledInstance(1120,630,Image.SCALE_DEFAULT);
+        ImageIcon i33 = new ImageIcon(i22);
+        JLabel image = new JLabel(i33);
+        image.setBounds(0,0,1120,630);
+        add(image);
+
+        setSize(1000,400);
+        setLocation(300,150);
         setLayout(null);
-        setLocation(300,50);
         setVisible(true);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == add){
-            String name = tname.getText();
-            String fname = tfname.getText();
-            String dob = ((JTextField) ((Object) tdob).getDateEditor().getUiComponent()).getText();
-            String salary = tsalary.getText();
-            String address = taddress.getText();
-            String aadhar = taadhar.getText();
-            String phone = tphone.getText();
-            String email = temail.getText();
-            String education = (String) Boxeducation.getSelectedItem();
-            String designation = tdesignation.getText();
-            String empID = tempid.getText();
-
-            try{
+        if (e.getSource()==delete){
+            try {
                 conn c = new conn();
-                String query = "insert into employee values('"+name+"', '"+fname+"', '"+dob+"', '"+salary+"','"+address+"', '"+phone+"', '"+email+"', '"+education+"', '"+designation+"','"+aadhar+"', '"+empID+"')";
+                String query = "delete from employee where empId = '"+choiceEMPID.getSelectedItem()+"'";
                 c.statement.executeUpdate(query);
-                JOptionPane.showMessageDialog(null,"Details added successfully");
+                JOptionPane.showMessageDialog(null,"Employee Deleted Sucessfully");
                 setVisible(false);
                 new Main_class();
 
             }catch (Exception E){
                 E.printStackTrace();
             }
-
         }else {
             setVisible(false);
-            new Main_class();
         }
     }
 
     public static void main(String[] args) {
         new RemoveEmployee();
-
     }
 }
--- src/employee/management/system/RemoveEmployeeEmployee.java
+++
@@ -1,5 +0,0 @@
-package employee.management.system;
-
-public class RemoveEmployeeEmployee {
-
-}
